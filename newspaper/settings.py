a3567f1010422ab37f217a9d79690785354f5366
MUSIC <agayevzakir239@gmail.com>
2019-02-22 15:10:41 +0000

    DONE

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import whitenoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'news/static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#added for production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#"""
